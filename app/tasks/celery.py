e29cd2cd9674b8ded3d7608c6c603b6a048caee0
jimmyliaoviva <jimmy.liao86@gmail.com>
2024-12-05 21:01:56 +0800

    FINX-1 Init celery and worker (#1)
    * init celery and worker
    
    * test
    
    * enable agent

---
+++ app/tasks/celery.py
@@ -0,0 +1,16 @@
+from celery import Celery
+
+app = Celery("tasks", broker_url='amqp://localhost:5672',
+result_expires=3600,
+task_serializer='json',
+result_serializer='json',
+accept_content=['json'],
+timezone='Asia/Taipei',
+enable_utc=True)
+
+app.conf.imports = [
+    'app.tasks.update_price',
+    'app.tasks.hello_world']
+
+if __name__ == '__main__':
+    app.start()
